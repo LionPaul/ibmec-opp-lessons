d012dd9151d0f8508c2ca8160909d13d130ef608
LionPaul <ptalregister@gmail.com>
2025-08-26 16:49:08 -0300

    Create HelloWorld.java

---
+++ ibmec-oop-lessons/src/br/edu/ibmec/oop/lessons/ex01/helloworld/HelloWorld.java
@@ -0,0 +1,11 @@
+package br.edu.ibmec.oop.lessons.ex01.helloworld;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+
+	}
+
+}
